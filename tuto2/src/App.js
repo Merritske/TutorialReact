5534078abab66f78e42a6b476f86ddcfb88f5082
Merritske <merritske@gmail.com>
2022-03-22 15:58:49 +0100

    updtae 22

--- tuto2/src/App.js
+++ tuto2/src/App.js
@@ -1,70 +1,108 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
+import "./styles/home.css"
 import { Button, Container, Form } from "react-bootstrap"
+import LoginForm from "./components/LoginForm"
 
 //logindata maken
 
 function App() {
-const [value, setValue] = useState(0)
-const [data, setData] = useState([{id:1}])
-const [formData, setFormData] = useState()
+  const [value, setValue] = useState(0)
+  const [data, setData] = useState([{ id: 1 }])
+  const [formData, setFormData] = useState()
 
-const handleClick = (e)=>{
-  e.preventDefault()
-  console.log(value)
-  //set state data here
-  //setValue(value +1) stat changed here
-  setData({...data, name: "An"})
-}
 
-const formHandler = (e)=>{
-//console.log(e.target)
-const {name, value} = e.target
-console.log(name, value)
-setFormData({
-  ...formData,
-  [name]: value
-})
-}
+  //BLOG data van doc into JSON
+  const [blogData, setBlogData] = useState([])
 
-const  formSubmitHandler = (e)=>{
-e.preventDefault()
-console.log(formData)
-}
+  let url =["https://www.googleapis.com/blogger/v3/blogs/8867027205370636728/posts/7983034314144341891?key=AIzaSyBBPq4WOAiBBlsdGnL8xNqB71S5BMEUMV4", 
+  "https://www.googleapis.com/blogger/v3/blogs/8867027205370636728/posts/1905931621331799833?key=AIzaSyBBPq4WOAiBBlsdGnL8xNqB71S5BMEUMV4"  ]
+
+ 
+useEffect(() => {
+for(let x = 0 ; x< url.length ; x ++){
+     fetch(url[x])
+      .then(res => res.json()) // is om de data om te zetten json file
+      .then(data => {
+        setBlogData(data)
+        //retrieved data here
+  
+})}
+
+  }, [])
+ console.log(blogData)
+//functie maken om data toe te voegen 
+//werkt niet zoals ik het eerst dacht
+
+
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(value)
+    //set state data here
+    //setValue(value +1) stat changed here
+    setData({ ...data, name: "An" })
+
+  }
+
+  //dit gebruiken in mijn app om importMenu te gebruiken
+  const formHandler = (e) => {
+    //console.log(e.target)
+    const { name, value } = e.target
+    console.log(name, value)
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+  //npm install json-server -g
+  //json-server --watch yourfile.json -p5000
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    //send data to server here
+    //application/json; charset=UTF-8
+    fetch("http://localhost:5000/db", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify(formData)
+    }
+    )
+  }
+  //GET retrieve data
+  //POST create data
+  //PUT (single object)(or PATCH) update data
+  //DELETE delete data
+
 
 
   return (
-   <>
-   <Container>
+    <>
+      <Container>
 
-         <Button onClick={()=>setData({...data, email: "mail.com"})} >Click </Button>
-   <Button onClick={handleClick}>Click </Button>
-   <h1 className="display-5">{value}</h1>
-{console.log(data)} 
+        <Button onClick={() => setData({ ...data, email: "mail.com" })} >Click </Button>
+        <Button onClick={handleClick}>Click </Button>
+        <h1 className="display-5">{value}</h1>
+        {console.log(data, "ggfrfrderde")}
 
-<Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" name="email" placeholder="Enter email" onChange={formHandler} />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+        <LoginForm submitHandler={formSubmitHandler} formHandler={formHandler} />
+<br/>
+<div className="blog">
+  <h1>
+   {blogData.title}
+</h1>
+       <br/>
+     
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" name="password" placeholder="Password" onChange={formHandler} />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit" onClick={formSubmitHandler}>
-    Submit
-  </Button>
-</Form>
-   </Container>
+        <div dangerouslySetInnerHTML={{ __html: blogData.content }} />
 
-   </>
+</div>
+
+      </Container>
+
+    </>
   );
 }
 
